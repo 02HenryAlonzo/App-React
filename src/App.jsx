a02ae0c85658aa06006dd32567ea434a84009176
Henry <henrycujcuj42@gmail.com>
2024-04-09 13:14:59 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export const HolaMundo = () => {
+    return (
+        <>
+            <h1>Nuevo Componente</h1>
+        </>
+    )
+}
